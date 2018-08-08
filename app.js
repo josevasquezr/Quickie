df807ecfacbd4919883a186f23d9d5954ad89258
josevasquezr <javierv.ramos@gmail.com>
2018-08-08 01:23:26 -0600

    Agregando estructura de controladores de usuario

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var bodyParser = require('body-parser');
 var app = express();
 
 //cargar rutas
+var user_routes = require('./routes/user');
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -13,6 +15,8 @@ app.use(bodyParser.json());
 //configurar cabeceras http
 
 //rutas base
+app.use('/api', user_routes);
+
 app.get('/pruebas', function(request, response){
     response.status(200).send({message: "Esto esta avanzando!"});
 });
